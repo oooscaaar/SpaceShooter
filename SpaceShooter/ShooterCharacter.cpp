601efed58350cb9ed3eb6f46ec4b9cca94bec48a
Oscar <empanaisimo@gmail.com>
2021-03-07 18:32:34 +0100

    Project initialised

---
+++ SpaceShooter/ShooterCharacter.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCharacter.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AShooterCharacter::MoveForward(float AxisValue) 
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue);	// Forward vector multiplied by the AxisValue (1 or -1) to move forward or backwards.
+}
+
+void AShooterCharacter::LookUp(float AxisValue) 
+{
+	AddControllerPitchInput(AxisValue * Sensitivity); // Sensitivity ranges from 0.1 to 1.
+}
+
+void AShooterCharacter::LookUpRate(float AxisValue) 
+{
+	AddControllerPitchInput(AxisValue * GetWorld()->GetDeltaSeconds() * RotationRate); // Multiplied by DeltaSeconds to make it framerate independent.
+}
+
+void AShooterCharacter::MoveRight(float AxisValue) 
+{
+	AddMovementInput(GetActorRightVector() * AxisValue);
+}
+
+void AShooterCharacter::LookRight(float AxisValue) 
+{
+	AddControllerYawInput(AxisValue * Sensitivity);
+}
+
+void AShooterCharacter::LookRightRate(float AxisValue) 
+{
+	AddControllerYawInput(AxisValue * GetWorld()->GetDeltaSeconds() * RotationRate); // Multiplied by DeltaSeconds to make it framerate independent.
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AShooterCharacter::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &AShooterCharacter::LookUpRate);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AShooterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &AShooterCharacter::LookRight);
+	PlayerInputComponent->BindAxis(TEXT("LookRightRate"), this, &AShooterCharacter::LookRightRate);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &ACharacter::Jump);
+}
+
